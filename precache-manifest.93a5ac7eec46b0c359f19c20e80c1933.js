8c38305db4037577c35324496813f3fcaa252564
Aswin Varghese <ashwin.vargheese@gmail.com>
2019-04-07 18:38:49 +0530

    Updates

---
+++ precache-manifest.93a5ac7eec46b0c359f19c20e80c1933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "393ab1423546b2ed805e",
+    "url": "/DG-movieapp/static/js/runtime~main.9a51798b.js"
+  },
+  {
+    "revision": "9e0ed4e7e5c93ffcec62",
+    "url": "/DG-movieapp/static/js/main.4a84784e.chunk.js"
+  },
+  {
+    "revision": "eadc27ef5bb806b755fe",
+    "url": "/DG-movieapp/static/js/2.0435adb0.chunk.js"
+  },
+  {
+    "revision": "9e0ed4e7e5c93ffcec62",
+    "url": "/DG-movieapp/static/css/main.6b69a718.chunk.css"
+  },
+  {
+    "revision": "d1e8f9945c3d5f9240428028fb1066be",
+    "url": "/DG-movieapp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/DG-movieapp/precache-manifest.93a5ac7eec46b0c359f19c20e80c1933.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/DG-movieapp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
